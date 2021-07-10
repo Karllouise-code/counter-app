23587c2b3baf6117d74786ce073007a092a5f584
Karl <karllouiserito08@gmail.com>
2021-07-10 15:30:09 +0800

    adjust slightly if statement

--- main.js
+++ main.js
@@ -3,7 +3,7 @@ let number = document.getElementById("number");
 let count = 0
 
 function decrement() {
-    if (count <= 0) {
+    if (count == 0) {
         alert('Count cannot be less than 0')
     } else {
         count--
@@ -12,7 +12,7 @@ function decrement() {
 }
 
 function increment() {
-    if (count >= 50) {
+    if (count == 50) {
         alert('Count cannot be greater than 50')
     } else {
         count++
