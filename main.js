965417e879e4ef9e87bfdec399c56491a30540f1
Karl <karllouiserito08@gmail.com>
2021-07-10 14:10:08 +0800

    initial counter app

---
+++ main.js
@@ -0,0 +1,21 @@
+let number = document.getElementById("number");
+
+let count = 0
+
+function decrement() {
+    if (count <= 0) {
+        alert('Count cannot be less than 0')
+    } else {
+        count--
+        number.innerText = count
+    }
+}
+
+function increment() {
+    if (count >= 50) {
+        alert('Count cannot be greater than 50')
+    } else {
+        count++
+        number.innerText = count
+    }
+}
