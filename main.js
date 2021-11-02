11dc48ef7819de26116c2acfbbcef41074b36700
Karl <karllouiserito08@gmail.com>
2021-11-02 14:26:06 +0800

    added left & right arrow function

--- main.js
+++ main.js
@@ -1,21 +1,41 @@
-let number = document.getElementById("number");
+let number = document.getElementById('number');
+let count = 0;
 
-let count = 0
+function addMinus(e) {
+    if (e.key === 'ArrowLeft') {
+        if (count == 0) {
+            alert('Count cannot be less than 0');
+        } else {
+            count--;
+            number.innerText = count;
+        }
+    }
+
+    if (e.key === 'ArrowRight') {
+        if (count == 50) {
+            alert('Count cannot be greater than 50');
+        } else {
+            count++;
+            number.innerText = count;
+        }
+    }
+}
 
 function decrement() {
     if (count == 0) {
-        alert('Count cannot be less than 0')
+        alert('Count cannot be less than 0');
     } else {
-        count--
-        number.innerText = count
+        count--;
+        number.innerText = count;
     }
 }
 
 function increment() {
     if (count == 50) {
-        alert('Count cannot be greater than 50')
+        alert('Count cannot be greater than 50');
     } else {
-        count++
-        number.innerText = count
+        count++;
+        number.innerText = count;
     }
 }
+document.addEventListener('keydown', addMinus);
